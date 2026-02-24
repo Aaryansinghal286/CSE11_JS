78d492a58bd58d0c62784a137238e0ee92175558
Aaryansinghal286 <aaryansinghal28@gmail.com>
2026-02-24 10:30:19 +0530

    js

--- script.js
+++ script.js
@@ -1,38 +1,38 @@
-console.log("Hello World!");
+// console.log("Hello World!");
 // document.write("Hello World!");
 // alert("Hello World!");
 
 
-// var num=10;
-// console.log(num);
-// console.log(typeof(num));
-// num=false;
-// console.log(num);
-// console.log(typeof(num));
-// num="Aaryan";
-// console.log(num);
-// console.log(typeof(num));
-// num=null;
-// console.log(num);
-// console.log(typeof(num));
-// num=undefined;
-// console.log(num);
-// console.log(typeof(num));
+var num=10;
+console.log(num);
+console.log(typeof(num));
+num=false;
+console.log(num);
+console.log(typeof(num));
+num="Aaryan";
+console.log(num);
+console.log(typeof(num));
+num=null;
+console.log(num);
+console.log(typeof(num));
+num=undefined;
+console.log(num);
+console.log(typeof(num));
 
 
 // function
-// var sayHello=function(){
-//     alert("Hello World!");
-// }
+var sayHello=function(){
+    alert("Hello World!");
+}
 // array
-// var numberarr=[1,2,3];
-// var animals= new Array("giraffe","LION","elephant");
+var numberarr=[1,2,3];
+var animals= new Array("giraffe","LION","elephant");
 // object
-// var person={
-//     name:"Aaryan",
-//     age:20,
-//     city:"Bangalore"
-// }
+var person={
+    name:"Aaryan",
+    age:20,
+    city:"Bangalore"
+}
 
 
 const sym1= Symbol(4);
@@ -44,3 +44,25 @@ if(sym1===sym2){
 }
 else{    console.log("False");
 }
+
+
+var x=6;
+var y=3;
+console.log(x=10 && y>1);
+console.log(x==6 || y==5);
+console.log(!(x==y));
+
+
+var i;
+for(i=0;i<3;i++){
+    console.log(i);
+}
+
+
+var count;
+document.write("Starting Loop" +"<br>");
+for(count=0;count<=5;count++){
+    document.write("Current Count" + count);
+    document.write("<br>");
+}
+document.write("Loop Ended");
