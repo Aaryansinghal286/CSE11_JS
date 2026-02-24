ce1e6e2f5358e0925bb85c534cac9b6f45005d18
Aaryansinghal286 <aaryansinghal28@gmail.com>
2026-02-24 09:44:41 +0530

    JS

--- script.js
+++ script.js
@@ -1,3 +1,46 @@
 console.log("Hello World!");
-document.write("Hello World!");
-alert("Hello World!");
+// document.write("Hello World!");
+// alert("Hello World!");
+
+
+// var num=10;
+// console.log(num);
+// console.log(typeof(num));
+// num=false;
+// console.log(num);
+// console.log(typeof(num));
+// num="Aaryan";
+// console.log(num);
+// console.log(typeof(num));
+// num=null;
+// console.log(num);
+// console.log(typeof(num));
+// num=undefined;
+// console.log(num);
+// console.log(typeof(num));
+
+
+// function
+// var sayHello=function(){
+//     alert("Hello World!");
+// }
+// array
+// var numberarr=[1,2,3];
+// var animals= new Array("giraffe","LION","elephant");
+// object
+// var person={
+//     name:"Aaryan",
+//     age:20,
+//     city:"Bangalore"
+// }
+
+
+const sym1= Symbol(4);
+console.log(sym1);
+const sym2= Symbol(4);
+console.log(sym2);
+if(sym1===sym2){
+    console.log("True");
+}
+else{    console.log("False");
+}
