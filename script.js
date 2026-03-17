b9444ec930d281e2a20cda89a481199f963f1fd0
Aaryansinghal286 <aaryansinghal28@gmail.com>
2026-03-17 10:14:20 +0530

    DOM usage

--- script.js
+++ script.js
@@ -3,60 +3,60 @@
 // alert("Hello World!");
 
 
-var num=10;
-console.log(num);
-console.log(typeof(num));
-num=false;
-console.log(num);
-console.log(typeof(num));
-num="Aaryan";
-console.log(num);
-console.log(typeof(num));
-num=null;
-console.log(num);
-console.log(typeof(num));
-num=undefined;
-console.log(num);
-console.log(typeof(num));
+// var num=10;
+// console.log(num);
+// console.log(typeof(num));
+// num=false;
+// console.log(num);
+// console.log(typeof(num));
+// num="Aaryan";
+// console.log(num);
+// console.log(typeof(num));
+// num=null;
+// console.log(num);
+// console.log(typeof(num));
+// num=undefined;
+// console.log(num);
+// console.log(typeof(num));
 
 
 // function
-var sayHello=function(){
-    alert("Hello World!");
-}
+// var sayHello=function(){
+//     alert("Hello World!");
+// }
 // array
-var numberarr=[1,2,3];
-var animals= new Array("giraffe","LION","elephant");
+// var numberarr=[1,2,3];
+// var animals= new Array("giraffe","LION","elephant");
 // object
-var person={
-    name:"Aaryan",
-    age:20,
-    city:"Bangalore"
-}
+// var person={
+//     name:"Aaryan",
+//     age:20,
+//     city:"Bangalore"
+// }
 
 
-const sym1= Symbol(4);
-console.log(sym1);
-const sym2= Symbol(4);
-console.log(sym2);
-if(sym1===sym2){
-    console.log("True");
-}
-else{    console.log("False");
-}
+// const sym1= Symbol(4);
+// console.log(sym1);
+// const sym2= Symbol(4);
+// console.log(sym2);
+// if(sym1===sym2){
+//     console.log("True");
+// }
+// else{    console.log("False");
+// }
 
 
-var x=6;
-var y=3;
-console.log(x=10 && y>1);
-console.log(x==6 || y==5);
-console.log(!(x==y));
+// var x=6;
+// var y=3;
+// console.log(x=10 && y>1);
+// console.log(x==6 || y==5);
+// console.log(!(x==y));
 
 
-var i;
-for(i=0;i<3;i++){
-    console.log(i);
-}
+// var i;
+// for(i=0;i<3;i++){
+//     console.log(i);
+// }
 
 
 // var count;
@@ -77,25 +77,72 @@ for(i=0;i<3;i++){
 // document.write("Loop stopped");
 
 
-var grade="0";
-switch(grade){
-    case "A":
-        console.log("Excellent");
-        break;
-    case "B":
-        console.log("Good");
-        break;
-    case "C":
-        console.log("Fair");
-        break;
-    case "D":
-        console.log("Poor");
-        break;
-    case "F":
-        console.log("Fail");
-        break;
-    default:
-        console.log("Invalid Grade");
-}
+// var grade="0";
+// switch(grade){
+//     case "A":
+//         console.log("Excellent");
+//         break;
+//     case "B":
+//         console.log("Good");
+//         break;
+//     case "C":
+//         console.log("Fair");
+//         break;
+//     case "D":
+//         console.log("Poor");
+//         break;
+//     case "F":
+//         console.log("Fail");
+//         break;
+//     default:
+//         console.log("Invalid Grade");
+// }
+
+
+// Mouse events
+
+// function onclick(){
+//     console.log("Button Clicked");
+// }
+// function onmouseut(){
+//     console.log("Mouse Out");
+// }
+// function onmouseover(){
+//     console.log("Mouse Over");
+// }
+// function onmouseup(){
+//     console.log("Mouse Up");
+// }
+// function onmousedown(){
+//     console.log("Mouse Down");
+// }
+// function onmousemove(){
+//     console.log("Mouse Move");
+// }
+
+
+// keyboard event
+
+// const x=document.querySelector("input");
+// x.addEventListener("keydown",function(event){
+//     console.log("Key Down");
+// });
+// x.addEventListener("keyup",function(event){
+//     console.log("Key Up");
+// });
+
+// form events
+// const x=document.querySelector("input");
+// x.addEventListener("focus",onhanddlefocus);
+// x.addEventListener("blur",onhanddleblur);
+
+// //windows events --> onbody, onunload, onresize
+// function bodyChange(){
+//     console.log("Changes done");
+// }
+
+
+//JSON
+// member[0].powers[2]
 
 
